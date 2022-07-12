a13c135450da1b4730cda98b14050a89e22af2c3
Devratb <93407364+Devratb@users.noreply.github.com>
2022-07-12 16:29:48 -0400

    node update

--- index.js
+++ index.js
@@ -25,13 +25,18 @@ mongoose.connect(url,connectionParams)
     })
 
 //middlewhere     
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
+//app.use(express.json());
 
-const database = {
-    "name": "Nathifa",
-    "lastName": "Oleana"
+//schema
+const schema = {
+    firstName: String,
+    lastName: String
 }
+
+//model
+const monModel = mongoose.model("name", schema);
 
 app.listen(
     PORT,
@@ -42,7 +47,29 @@ app.get('/data', (req, res) =>
     res.status(200).json(database));
 
 app.post('/user', (req, res) => {
-    const user = req.body.user
+    if (!req.body.name) return res.status(400).send("Name Not received");
+    if (!req.body.lastName) return res.status(400).send("Last Name Not received");
+    
+    console.log("User Add request received for " + req.body.name);
+
+    // Cleanup Unnessesary shit pls
+    console.log(req.body);
+    console.error(req.data);
+    const user = req.body.name
+
+    console.log(req.body.name);
+    
+
+    const data = new monModel({
+        firstName:req.body.name,
+        lastName:req.body.lastName
+    });
+
+    // Log to console that you saved to db successfully pls
+    const val = data.save();
+     
+
+    // Create a response Object outside of the res function pls
     res.status(201).json({
         message: 'Post created successfully!',
         post: {
@@ -50,5 +77,6 @@ app.post('/user', (req, res) => {
           user: user,
         }
       });
+      console.log(req.body);
     }); 
     
